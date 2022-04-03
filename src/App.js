cadbb195a90de74213907658bb0348c55b7074e4
David Hinote <drhinote@gmail.com>
2022-04-03 13:38:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,14 +26,7 @@ import Minesweeper from './Components/Minesweeper/Minesweeper';
 //DCM(1a) -+ import {ColorList, ColorBox} from './Components/ComponentImportList';
 //DCM(1b) -- The Code in comment 1a is an alternative way of importing all component pieces in fewer lines of code using the file ComponentImportList.js.
 
-
-export default function App() {
-  const [route, setRoute] = useState(null);
-
-
-  const router = {
-    setRoute,
-    routes: {
+const routes = {
         Jili:  <Names />,
         Christopher: <ColorSlider />,
         Aaron: <TicTacToe />,
@@ -46,9 +39,17 @@ export default function App() {
         Alex:  <AlexList />,
         Frank: <ColorClick />,
         Emuhit: <Minesweeper/>
-     }
+     };
+
+export default function App() {
+  const [route, setRoute] = useState(null);
+
+  const Router = (routesObj, updateRoute) => {
+    this.routes = Object.keys(routesObj);
+    this.navigate = (key)=>updateRoute(routesObj[key]);
   };
-    
+  
+  const router = new Router(routes, setRoute);
 
   return (
     <div>
