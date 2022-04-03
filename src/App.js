c206ab7d2fdd436e13acccc945c48f09dc545bb8
David Hinote <drhinote@gmail.com>
2022-04-03 16:57:42 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-
-   
-    
-    
-  
-
-
-
-
-
 import React, { useState } from 'react';
 import './style.css';
-import Navbar from './Components/Navbar/Navbar';
-import Names from './Components/Names/Names';
-import ColorSlider from './Components/ColorList/Slider/ColorSlider';
-import ColorBox from './Components/ColorBox/ColorBox';
-import TicTacToe from './Components/TicTacToe/TicTacToe';
-import MovieForm from './Components/LukasPage/movieForm';
-import CircleClicker from './Components/CircleClicker/CircleClicker.jsx';
-import NameForm from './Components/NameForm/Form'
-import App21 from "./Components/App21/App21";
-import AlexList from './Components/AlexList/AlexList';
-import ColorClick from './Components/ColorClick/ColorClick';
-import DakotaPage from './Components/DakotaPage/DakotaPage';
-import Minesweeper from './Components/Minesweeper/Minesweeper';
-//DCM(1a) -+ import {ColorList, ColorBox} from './Components/ComponentImportList';
-//DCM(1b) -- The Code in comment 1a is an alternative way of importing all component pieces in fewer lines of code using the file ComponentImportList.js.
+import { Navbar, Names, ColorSlider, ColorBox, TicTacToe, MovieForm, CircleClicker, NameForm, App21, AlexList, ColorClick, DakotaPage, Minesweeper } from './Components/Common';
 
 const routes = {
         Jili:  <Names />,
