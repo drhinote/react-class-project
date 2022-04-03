e4f29c4ad8ab10994e102dbb8c95ba378ed6b2a0
David Hinote <drhinote@gmail.com>
2022-04-03 17:00:31 -0500

    Update ComponentImportList.js

--- src/Components/ComponentImportList.js
+++ src/Components/ComponentImportList.js
@@ -1,3 +1,15 @@
-export { default as ColorBox } from './ColorBox/ColorBox';
-export { default as ColorList } from './ColorList/ColorList';
-export {default as Navbar } from './Navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
+import Names from './Components/Names/Names';
+import ColorSlider from './Components/ColorList/Slider/ColorSlider';
+import ColorBox from './Components/ColorBox/ColorBox';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+import MovieForm from './Components/LukasPage/movieForm';
+import CircleClicker from './Components/CircleClicker/CircleClicker.jsx';
+import NameForm from './Components/NameForm/Form'
+import App21 from "./Components/App21/App21";
+import AlexList from './Components/AlexList/AlexList';
+import ColorClick from './Components/ColorClick/ColorClick';
+import DakotaPage from './Components/DakotaPage/DakotaPage';
+import Minesweeper from './Components/Minesweeper/Minesweeper';
+
+export default Navbar, Names, ColorSlider, ColorBox, TicTacToe, MovieForm, CircleClicker, NameForm, App21, AlexList, ColorClick, DakotaPage, Minesweeper;
